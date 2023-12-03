524ab0bcf956af02a81556bb6461753788fe32e8
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-12-03 14:36:32 +0300

    create user model

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/AkifhanIlgaz/random-question-selector/cfg"
+)
+
+func main() {
+	config, err := cfg.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(config)
+}

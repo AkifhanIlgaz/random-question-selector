6895aa95a6ec8d50e372e20d378ce57dec7db0f1
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-12-04 14:42:47 +0300

    hash password

--- services/auth.service.go
+++ services/auth.service.go
@@ -3,6 +3,8 @@ package services
 import "github.com/AkifhanIlgaz/random-question-selector/models"
 
 type AuthService interface {
-	SignUpUser(*models.SignUpInput) (*models.User, error)
-	SignInUser(*models.SignInInput) (*models.User, error)
+	SignUp(*models.SignUpInput) (*models.User, error)
+	SignIn(*models.SignInInput) (*models.User, error)
 }
+
+

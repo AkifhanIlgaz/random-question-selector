d53412593d41ca37c1ea3925112d4231e4c8841c
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-12-06 09:55:32 +0300

    stub out question service functions

--- models/question.go
+++ models/question.go
@@ -1,1 +1,10 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Question struct {
+	Id           primitive.ObjectID `json:"id" bson:"_id"`
+	Text         string             `json:"text" bson:"text" binding:"required"`
+	Answer       string             `json:"answer" bson:"answer" binding:"required"`
+	AnswerSource string             `json:"answerSource" bson:"answerSource" binding:"required"`
+}
---
+++ services/question.go
@@ -0,0 +1,44 @@
+package services
+
+import (
+	"context"
+
+	"github.com/AkifhanIlgaz/random-question-selector/models"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type QuestionService struct {
+	db  *mongo.Database
+	ctx context.Context
+}
+
+func NewQuestionService(ctx context.Context, db *mongo.Database) *QuestionService {
+	return &QuestionService{
+		db:  db,
+		ctx: ctx,
+	}
+}
+
+func (service *QuestionService) AddQuestion(question models.Question) error {
+	return nil
+}
+
+func (service *QuestionService) DeleteQuestion(questionId string) error {
+	return nil
+}
+
+func (service *QuestionService) GetQuestion(questionId string) (models.Question, error) {
+	return models.Question{}, nil
+}
+
+func (service *QuestionService) EditQuestion(questionId string, updatedQuestion models.Question) error {
+	return nil
+}
+
+func (service *QuestionService) GetAllQuestionsOfGroup(group string) ([]models.Question, error) {
+	return nil, nil
+}
+
+func (service *QuestionService) GetRandomQuestionsByGroup(group string, count int) ([]models.Question, error) {
+	return nil, nil
+}

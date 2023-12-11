2d937413d16d8c2891df8d7ad538df33927c97b6
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-12-11 11:06:57 +0300

    update response with status message

--- utils/response.go
+++ utils/response.go
@@ -3,19 +3,25 @@ package utils
 import (
 	"strings"
 
+	"github.com/AkifhanIlgaz/random-question-selector/models"
 	"github.com/gin-gonic/gin"
 )
 
-func BuildReponseStatusMessage(status string, message string) gin.H {
+func ResponseWithStatusMessage(ctx *gin.Context, code int, status models.Status, message string) {
 	message = strings.TrimSpace(message)
-	if message == "" {
-		return gin.H{
-			"status": status,
-		}
-	} else {
-		return gin.H{
-			"status":  status,
-			"message": message,
-		}
+	statusMessage := models.StatusMessage(status)
+
+	h := gin.H{
+		"status": statusMessage,
+	}
+	if message != "" {
+		h["message"] = message
+	}
+
+	switch status {
+	case models.StatusFail:
+		ctx.AbortWithStatusJSON(code, h)
+	case models.StatusSuccess:
+		ctx.JSON(code, h)
 	}
 }
